6195bee64023401d21f11697d677c5f86bc84f86
jaeseok an <k1462487@kcl.ac.uk>
2016-02-14 03:10:32 +0000

    mysite

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'admin/static'),
+    os.path.join(PROJECT_ROOT, 'mysite/static'),
 )
 
 # Simplified static file serving.
