70eedc0051db59b3084c471d8b7ce845de48b326
jaeseok an <k1462487@kcl.ac.uk>
2016-02-14 02:41:35 +0000

    whitenoises removed

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,3 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
