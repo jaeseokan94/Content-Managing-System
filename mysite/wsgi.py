90c9b4e102c6b48e673e3fc85b8225a56b017c8f
jaeseok an <k1462487@kcl.ac.uk>
2016-02-14 02:39:42 +0000

    wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,4 +15,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
