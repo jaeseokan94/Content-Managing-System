1877722dff34432243920b4a5969096cdfb7d4cc
janaldous <jat.torres@gmail.com>
2016-02-16 19:55:18 +0000

    first draft backend completed

--- polls/admin.py
+++ polls/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 
-from .models import Language, Topic, SituationalVideo, LectureVideo, Exercise
+from .models import Language, Topic, SituationalVideo, Exercise, LanguageTopic, LanguageSubtopic, ExerciseQuestion
 
 admin.site.register(Language)
 
-@admin.register(Topic)
-class TopicAdmin(admin.ModelAdmin):
-	list_filter = ('language',)
-
+admin.site.register(Topic)
+admin.site.register(LanguageTopic)
+admin.site.register(LanguageSubtopic)
 admin.site.register(SituationalVideo)
-admin.site.register(LectureVideo)
 admin.site.register(Exercise)
+admin.site.register(ExerciseQuestion)
