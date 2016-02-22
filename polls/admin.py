0eb0d442c100bb3b49ded25a6a52164fd38dea82
jaeseok an <k1462487@kcl.ac.uk>
2016-02-22 21:04:34 +0000

    revert to working version 83063ed03ab1acbd1c8f1c911ca1afbcdd4f8638

--- polls/admin.py
+++ polls/admin.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 
-from .models import Language, Topic, SituationalVideo, LectureVideo, Exercise, Dialect, Resource, ResourceItem, ResourceItemPicture
+from .models import Language, Topic, SituationalVideo, Exercise, LanguageTopic, LanguageSubtopic, ExerciseQuestion
 
 admin.site.register(Language)
 
-@admin.register(Topic)
-class TopicAdmin(admin.ModelAdmin):
-	pass
-
+admin.site.register(Topic)
+admin.site.register(LanguageTopic)
+admin.site.register(LanguageSubtopic)
 admin.site.register(SituationalVideo)
-admin.site.register(LectureVideo)
 admin.site.register(Exercise)
-admin.site.register(Dialect)
-admin.site.register(Resource)
-admin.site.register(ResourceItem)
-admin.site.register(ResourceItemPicture)
+admin.site.register(ExerciseQuestion)
--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,8 @@ from django.contrib import admin
 
 
 
+
+
 from . import views
 
 
@@ -17,10 +19,26 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
-	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    # ex: /polls/5/
+    url(r'^language/$', views.language_list, name='language_list'),
+    url(r'^language/(?P<language_name>[a-zA-Z0-9]+)', views.language_detail, name='language_detail'),
+    url(r'^topics/$', views.topic_list, name='topic_list'),
+    #url(r'^list/$', 'list', name='list'),
+    url(r'^(?P<language>[a-zA-Z0-9]+)/(?P<level>[a-zA-Z0-9]+)/(?P<topic_name>[a-zA-Z0-9]+)/situationalVideo/st/$', views.situational_video_list),
+    url(r'^(?P<language>[a-zA-Z0-9]+)/(?P<level>[a-zA-Z0-9]+)/(?P<topic_name>[a-zA-Z0-9]+)/(?P<subtopic_name>[a-zA-Z0-9]+)/grammarVideo/$', views.grammar_video_list),
+    url(r'^(?P<language>[a-zA-Z0-9]+)/(?P<level>[a-zA-Z0-9]+)/(?P<topic_name>[a-zA-Z0-9]+)/(?P<subtopic_name>[a-zA-Z0-9]+)/exerciseQuestion/$', views.exercise_question_list),
+
+
+
 ]
+
+
 if not settings.DEBUG:
     urlpatterns += patterns(
 '',
- (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )  
+
