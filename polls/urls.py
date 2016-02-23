a5a5448a5d01a10833f208fef67c849485e5d6d6
janaldous <jat.torres@gmail.com>
2016-02-23 15:29:04 +0000

    removed Choice and Questions from models, urls and views

--- polls/urls.py
+++ polls/urls.py
@@ -15,10 +15,6 @@ admin.autodiscover()
 
 app_name='polls'
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
-from polls.models import Language
+
 from polls.serializers import LanguageSerializer
 from polls.serializers import SituationalVideoSerializer , GrammarVideoSerializer , ExerciseQuestionSerializer
 from polls.serializers import SituationalVideo
-from polls.models import LanguageSubtopic, ExerciseQuestion, Exercise
+
 
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
@@ -18,7 +18,7 @@ from django.utils import timezone
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Choice, Question, Language, Topic, LanguageTopic
+from polls.models import Language, Topic, LanguageTopic, LanguageSubtopic, ExerciseQuestion, Exercise
 
 
 class JSONResponse(HttpResponse):
@@ -73,51 +73,6 @@ def snippet_detail(request, pk):
     elif request.method == 'DELETE':
         language.delete()
         return HttpResponse(status=204)
-
-
-
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
 
 def language_list(request):
     language_list = Language.objects.all()
