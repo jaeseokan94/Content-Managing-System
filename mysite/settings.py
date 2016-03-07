d1680908de0b7c21fe53e3a6776dca4feaea49ca
janaldous <jat.torres@gmail.com>
2016-03-07 19:04:01 +0000

    commented out dj database code

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
