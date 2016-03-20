e5ca656559e4357f27fad5979bd1d7786ef41467
janaldous <jat.torres@gmail.com>
2016-03-20 15:46:53 +0000

    added login redirect url to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,5 +159,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 #Django registration redux settings
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
-
 SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
