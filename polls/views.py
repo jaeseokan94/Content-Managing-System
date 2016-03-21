ba2f6aa4077fec55289e8bf382492a9c963bd183
janaldous <jat.torres@gmail.com>
2016-03-21 11:07:30 +0000

    cleaned html

--- polls/views.py
+++ polls/views.py
@@ -226,12 +226,11 @@ def language_topic_list(request, language_name, level):
     level = Level.objects.get(level=level)
     topics = Topic.objects.filter(level=level.id)
 
-
     context = {
         'topic_list': topics,
-       'language_name': language_name,
-       'level': level,
-       'language' : language,
+        'language_name': language_name,
+        'level': level,
+        'language' : language,
     }
 
     return render(request, 'polls/topiclist.html', context)
