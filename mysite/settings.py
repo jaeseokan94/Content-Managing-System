a1087073cdab5f698e291c1d3bcf7b6d5c272b2d
janaldous <jat.torres@gmail.com>
2016-03-23 05:06:20 +0000

    added whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,3 +177,8 @@ LOGIN_EXEMPT_URLS = (
     r'^media/',
     r'^static/'
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
