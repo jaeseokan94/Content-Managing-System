289d6b9754a622db4cd3e3649bfd9cd6ccb397fb
janaldous <jat.torres@gmail.com>
2016-03-24 13:11:43 +0000

    fixed exercise_vocab_question_create

--- polls/views.py
+++ polls/views.py
@@ -858,7 +858,7 @@ def exercise_vocab_question_create(request, language_name, level, topic_name, su
     language = Language.objects.get(name=language_name)
     topic = Topic.objects.get(topic_name=topic_name)
     language_topic = LanguageTopic.objects.filter(language=language.id).get(topic=topic.id)
-    subtopic = LanguageSubtopic.objects.filter(language_topic=language_topic.id)
+    subtopic = LanguageSubtopic.objects.filter(language_topic=language_topic.id).get(subtopic_name=subtopic_name)
     exercise = Exercise.objects.get(id=subtopic.id)
 
     form = ExerciseVocabularyQuestionForm(request.POST or None)
